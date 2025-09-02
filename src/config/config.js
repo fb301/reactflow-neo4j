b966e4917940340ca8cf9ef26a17a216d142dd52
fb301 <fb301@proton.me>
2025-09-02 12:50:30 +0200

    removed files

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ class Config {
   constructor() {
     this.APP_NAME = import.meta.env.VITE_APP_NAME || "ReactFlow Neo4j";
     this.GRAPHQL_URL =
-      import.meta.env.VITE_GRAPHQL_URL || "http://localhost:8000/graphql";
+      import.meta.env.VITE_GRAPHQL_URL || "http://127.0.0.1:8000/graphql";
   }
 }
 
