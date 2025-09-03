bad77237d748896845dca7559ecb375af40500bd
fb301 <fb301@proton.me>
2025-09-03 14:18:32 +0200

    edited client.js

--- src/gql/client.js
+++ src/gql/client.js
@@ -1,12 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { config } from "../config/config";
 
-const httpLink = new HttpLink({
-  uri: config.GRAPHQL_URL,
-});
-
 export const client = new ApolloClient({
-  link: httpLink,
+  link: new HttpLink({ uri: config.GRAPHQL_URL }),
   cache: new InMemoryCache(),
   queryDeduplication: false,
   defaultOptions: {
