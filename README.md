212036bd54acc8a2c0228821df597710a146a214
fb301 <fb301@proton.me>
2025-09-10 16:42:00 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-The purpose of this project is to transfer data between [ReactFlow](https://reactflow.dev/) and [Neo4j](https://neo4j.com/) using [Flask](https://flask.palletsprojects.com/en/stable/) as an API.
+The purpose of this project is to transfer data between [ReactFlow](https://reactflow.dev/) and [Neo4j](https://neo4j.com/) using [FastAPI](https://fastapi.tiangolo.com/) with [Strawberry-GraphQL](https://strawberry.rocks/) API.
--- api/gql/__init__.py
+++ api/gql/__init__.py
@@ -1,15 +1,3 @@
-"""
-GraphQL Package for ReactFlow-Neo4j Application
-
-This package contains the GraphQL schema, resolvers, and main application
-for handling the API between ReactFlow frontend and Neo4j database.
-
-Modules:
-    - schemas: Strawberry type definitions and input types
-    - resolvers: Business logic for GraphQL operations
-    - gql: Main GraphQL application and schema setup
-"""
-
 from .gql import graphql_app, schema
 from .schemas import Node, Relationship, FlowData, NodeInput, RelationshipInput, FlowDataInput
 
