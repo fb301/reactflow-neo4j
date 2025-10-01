4cc9ecc8a8146d18419571192b7d14e85831b3b3
Fredrik <142401116+fb301@users.noreply.github.com>
2025-10-01 09:50:54 +0200

    Update Neo4j connection credentials and database name

--- Server/database/database.py
+++ Server/database/database.py
@@ -10,9 +10,9 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 url = os.getenv("NEO4J_URI", "neo4j://127.0.0.1:7687")
 username = os.getenv("NEO4J_USER", "neo4j")
-password = os.getenv("NEO4J_PASSWORD", "Hejsan1!")
+password = os.getenv("NEO4J_PASSWORD", "your_passwd")
 neo4j_version = os.getenv("NEO4J_VERSION", "2025.08.0")
-database = os.getenv("NEO4J_DATABASE", "test")
+database = os.getenv("NEO4J_DATABASE", "your_database")
 
 port = int(os.getenv("PORT", 8080))
 
